a701d86a9bca3deaee413a9789685bb652fc5e47
Ignacio Garcia <ignacioagustin.garcia@uala.com.ar>
2022-10-02 12:57:43 -0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        primary: colors.teal,
+        back: colors.slate,
+        ...colors
+    },
+    fontFamily: {
+      'sans': ['Poppins', 'Ubuntu', 'Tahoma', '"Helvetica Neue"', ...defaultTheme.fontFamily.sans]
+    }
   },
   plugins: [],
 }
