6f519dca8886bfea324205063bb01ef3113d6c31
Ignacio Garcia <ignacioagustin.garcia@uala.com.ar>
2022-10-04 19:14:28 -0300

    refactor: extract loged shell to file

---
+++ src/pages/Shell/LogedShell.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import { Configuration } from '../Configuration/Configuration'
+import { Records } from '../Records/Records'
+import { Sidebar } from './Sidebar'
+import { Footer } from './Footer'
+
+export const LogedShell = () => {
+    return (
+        <>
+            <Sidebar />
+            <main className='mb-16 ml-60'>
+                <Routes>
+                    <Route exact strict path='' element={ <div>home</div> }/>
+                    <Route exact strict path='registros' element={ <Records /> }>
+                        <Route exact strict path='egresos' element={ <div>ingresos</div> }/>
+                        <Route exact strict path='ingresos' element={ <div>egresos</div> }/>
+                    </Route>
+                    <Route exact strict path='configuracion' element={ <Configuration /> }>
+                        <Route exact strict path='categorias' element={ <div>cat</div> }/>
+                        <Route exact strict path='etiquetas' element={ <div>tag</div> }/>
+                        <Route exact strict path='billeteras' element={ <div>wall</div> }/>
+                        <Route exact strict path='telegram' element={ <div>tg</div> }/>
+                    </Route>
+                </Routes>
+            </main>
+            <Footer />
+        </>
+    )
+}
--- src/pages/Shell/Shell.jsx
+++ src/pages/Shell/Shell.jsx
@@ -1,13 +1,10 @@
 import React, { useContext } from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Configuration } from '../Configuration/Configuration'
-import { Records } from '../Records/Records'
-import { Sidebar } from './Sidebar'
-import { Footer } from './Footer'
+import { BrowserRouter } from "react-router-dom"
 import { UserContext } from '../../contexts/UserContext';
+import { LogedShell } from './LogedShell'
 
 export const Shell = () => {
-    const {user, saveUser } = useContext(UserContext)
+    const { user, saveUser } = useContext(UserContext)
 
     const setUser = () => {
         saveUser({
@@ -20,25 +17,11 @@ export const Shell = () => {
     return (
         <BrowserRouter className='flex'>
             { user && user.token? 
-                <>
-                <Sidebar />
-                <main className='mb-16 ml-60'>
-                    <Routes>
-                        <Route exact strict path='' element={ <div>home</div> }/>
-                        <Route exact strict path='registros' element={ <Records /> }>
-                            <Route exact strict path='egresos' element={ <div>ingresos</div> }/>
-                            <Route exact strict path='ingresos' element={ <div>egresos</div> }/>
-                        </Route>
-                        <Route exact strict path='configuracion' element={ <Configuration /> }>
-                            <Route exact strict path='categorias' element={ <div>cat</div> }/>
-                            <Route exact strict path='etiquetas' element={ <div>tag</div> }/>
-                            <Route exact strict path='billeteras' element={ <div>wall</div> }/>
-                            <Route exact strict path='telegram' element={ <div>tg</div> }/>
-                        </Route>
-                    </Routes>
-                </main>
-                <Footer />
-                </> : <button onClick={setUser} className="block my-5 mx-auto text-back-200 bg-back-400 p-3 rounded-lg"> set user </button>
+                <LogedShell /> 
+                : 
+                <button onClick={setUser} className="block my-5 mx-auto text-back-200 bg-back-400 p-3 rounded-lg"> 
+                    set user 
+                </button>
             }
         </BrowserRouter>
     )
