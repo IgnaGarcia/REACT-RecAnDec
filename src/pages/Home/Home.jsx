704cdb8324512789dde33073ad38ebdb6030391f
Ignacio Garcia <ignacioagustin.garcia@uala.com.ar>
2022-10-07 20:47:30 -0300

    added: plots in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,7 +9,20 @@ export const Home = () => {
             <Balance />
             <LinesPlot title="Historial de Ingresos y Egresos" groupBy="isOut" 
                 filterList={null} />
+
             <PiePlot title="Gastos por Categoria" groupBy="category" 
+                filterList={null} />
+            <LinesPlot title="Historial por Categorias" groupBy="category" 
+                filterList={null} />
+
+            <PiePlot title="Gastos por Etiqueta" groupBy="tags" 
+                filterList={null} />
+            <LinesPlot title="Historial por Etiquetas" groupBy="tags" 
+                filterList={null} />
+
+            <PiePlot title="Gastos por Billetera" groupBy="wallet" 
+                filterList={null} />
+            <LinesPlot title="Historial por Billetera" groupBy="wallet" 
                 filterList={null} />
         </div>
     )
