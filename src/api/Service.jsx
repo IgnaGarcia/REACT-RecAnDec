904e2cf04a6bfaffc8f4944411c5858432333577
Ignacio Garcia <ignacioagustin.garcia@uala.com.ar>
2022-10-06 22:46:48 -0300

    added: balance component

--- src/api/Service.jsx
+++ src/api/Service.jsx
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://api-rec-an-dec.vercel.app"
+// export const BASE_URL = "https://api-rec-an-dec.vercel.app"
+export const BASE_URL = "http://localhost:3100"
--- src/pages/Home/Balance.jsx
+++ src/pages/Home/Balance.jsx
@@ -5,16 +5,31 @@ import { useFetch } from '../../hooks/useFetch';
 
 export const Balance = () => {
     const { user, saveUser } = useContext(UserContext)
-    const balanceResponse = useFetch(getBalance(user))
+    const { loading, body, error } = useFetch(getBalance(user))
 
     return (
         <>
         {
-            balanceResponse.loading? "Cargando..." :
-                <div>
-                    {
-                        balanceResponse.body.data.map(el => <div>[{el._id.year}/{el._id.month}] - Income:{el.income}({el.countIncome}) - Expense:{el.expense}({el.countExpense})</div>)
-                    }
+            loading? "Cargando..." :
+                <div className='card py-8'>
+                    <div className='flex justify-between items-center mb-6'>
+                        <h2 className='text-xl'> Balance del mes {body.data[0]._id.month}/{body.data[0]._id.year}</h2>
+                        <h3 className='text-3xl font-semibold'> { body.data[0].income - body.data[0].expense } </h3>
+                    </div>
+                    <div className='flex justify-between'>
+                        <div className='w-2/5'>
+                            <h4 className='text-lg font-semibold mb-3'> Egresos </h4>
+                            <p className='flex justify-between mb-1'> Mes Actual: <span>{body.data[0].expense}</span> </p>
+                            <p className='flex justify-between mb-1'> Mes Anterior: <span>{body.data[1].expense}</span> </p>
+                            <p className='flex justify-between mb-1'> Promedio: <span>{ (body.data[2].expense / body.data[2].countExpense).toFixed(2) }</span> </p>
+                        </div>
+                        <div className='w-2/5'>
+                            <h4 className='text-lg font-semibold mb-3'> Ingresos </h4>
+                            <p className='flex justify-between mb-1'> Mes Actual: <span>{body.data[0].income}</span> </p>
+                            <p className='flex justify-between mb-1'> Mes Anterior: <span>{body.data[1].income}</span> </p>
+                            <p className='flex justify-between mb-1'> Promedio: <span>{ (body.data[2].income / body.data[2].countIncome).toFixed(2) }</span> </p>
+                        </div>
+                    </div>
                 </div>
         }
         </>
