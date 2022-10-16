89cd8bbfa3cc799743662eff7d0b61171c27b82b
Ignacio Garcia <ignacioagustin.garcia@uala.com.ar>
2022-10-16 02:22:48 -0300

    wallet page

--- src/pages/Configuration/wallets/CreateWalletModal.jsx
+++ src/pages/Configuration/wallets/CreateWalletModal.jsx
@@ -3,53 +3,59 @@ import { UserContext } from '../../../contexts/UserContext'
 import { ConfigContext } from '../../../contexts/ConfigContext'
 import { useLazyFetch } from '../../../hooks/useLazyFetch'
 import { Modal } from '../../../components/Modal'
-import { postTag } from '../../../api/TagsService'
+import { postWallet } from '../../../api/WalletService'
 import { useForm } from '../../../hooks/useForm'
 
-export const CreateTagModal = ({ toggleOpen, isNew }) => {
+export const CreateWalletModal = ({ toggleOpen, isNew }) => {
   const { user } = useContext(UserContext)
-  const { tags, saveTags } = useContext(ConfigContext)
+  const { wallets, saveWallets } = useContext(ConfigContext)
   const { formState, onInputChange } = useForm({
     label: "",
-    alias: ""
+    alias: "",
+    acum: 0
   })
   const [formError, setError] = useState(null)
-  const tagResponse = useLazyFetch()
+  const response = useLazyFetch()
 
-  const saveTag = () => {
-    tagResponse.run(postTag(user, formState))
+  const saveWallet = () => {
+    response.run(postWallet(user, formState))
   }
 
   useEffect(() => {
-    if(!tagResponse.loading && (tagResponse.error || tagResponse.body)){
-        if (tagResponse.error) {
+    if(!response.loading && (response.error || response.body)){
+        if (response.error) {
             alert("Error al Enviar");
-        } else if (tagResponse.body.code === 11000) {
-            setError("Nombre o Alias ya existente entre tus etiquetas")
+        } else if (response.body.code === 11000) {
+            setError("Nombre o Alias ya existente entre tus billeteras")
         } else {
-            alert("Etiqueta Creada!");
-            tags.data.push(tagResponse.body.data)
-            saveTags(tags.data)
+            alert("Billetera Creada!");
+            wallets.data.push(response.body.data)
+            saveWallets(wallets.data)
             isNew(true)
             toggleOpen(false)
         }
     }
-}, [tagResponse.loading])
+}, [response.loading])
 
   return (
-    <Modal onPost={saveTag} toggleOpen={toggleOpen}>
-        <h2 className='title mb-8'> Crear Etiqueta </h2>
+    <Modal onPost={saveWallet} toggleOpen={toggleOpen}>
+        <h2 className='title mb-8'> Crear Billetera </h2>
         
-        <div className='w-1/2 m-auto text-right'>
+        <div className='w-3/5 m-auto text-right'>
             <div className='mb-3'>
                 <label htmlFor="label" className='mr-6'>Nombre:</label>
                 <input placeholder="Nombre" name="label" id="label" 
                     value={formState.label} onChange={onInputChange} required/>
             </div>
-            <div className='mb-8'>
+            <div className='mb-3'>
                 <label htmlFor="alias" className='mr-6'>Alias:</label>
                 <input placeholder="Alias" name="alias" id="alias" 
                     value={formState.alias} onChange={onInputChange} required/>
+            </div>
+            <div className='mb-8'>
+                <label htmlFor="acum" className='mr-6'>Dinero Actual:</label>
+                <input type="number" min={1} placeholder="Dinero" name="acum" id="acum" 
+                    value={formState.acum} onChange={onInputChange}/>
                 {formError? 
                     <div className='mt-2 text-xs text-center text-red-600'> {formError} </div> 
                 : ""}
---
+++ src/pages/Configuration/wallets/Wallets.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { getWallets } from '../../../api/WalletService'
+import { Chip } from '../../../components/Chip'
+import { CreateWalletModal } from './CreateWalletModal'
+import { ConfigurationTemplate } from '../ConfigurationTemplate'
+
+export const Wallets = () => {
+  const getDate = (dateStr) => {
+    let date = new Date(dateStr)
+    return `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`
+  }
+
+  const tableHead = <tr>
+    <th className='rounded-tl-xl'> Nombre </th>
+    <th className='border-l border-back-500'> Alias </th>
+    <th className='border-l border-back-500'> Dinero Actual </th>
+    <th className='border-l border-back-500 rounded-tr-xl'> Fecha de Creacion </th>
+  </tr>
+
+  const tableBody = (el, idx) => {
+    return <tr className='h-7' key={`tag-${idx}`}>
+        <td> 
+          <div className='flex justify-center items-center content-center flex-wrap'>
+            <Chip index={idx} label={el.label}/> 
+          </div>
+        </td>
+        <td className='border-l h-full'> 
+          <div className='flex justify-center items-center content-center flex-wrap'>
+            <Chip index={idx} label={el.alias}/>
+          </div>
+        </td>
+        <td className='border-l h-full'> {el.acum} </td>
+        <td className='border-l h-full'> {getDate(el.createDate)} </td>
+    </tr>
+  }
+
+  return (
+    <ConfigurationTemplate CreateModal={CreateWalletModal}  tableHead={tableHead} tableBody={tableBody} get={getWallets}
+      pieTitle={"Gastos por Billetera"} linesTitle={"Historial por Billetera"} groupBy={"wallet"} 
+      title={"Billetera"} createTitle={"Nueva Billetera"}/>
+  )
+}
