76f04c3a11added80003a6b582d8ae7af58b9ff8
Ignacio Garcia <ignacioagustin.garcia@uala.com.ar>
2022-10-19 00:53:06 -0300

    add initial text of telegram

--- src/pages/Configuration/telegram/Telegram.jsx
+++ src/pages/Configuration/telegram/Telegram.jsx
@@ -59,12 +59,26 @@ export const Telegram = () => {
   return (
     <>
       {user.telegramId[0] === '_'?
-       <div className='w-10/12 px-4 pt-12 mx-auto flex justify-evenly items-center text-lg'> 
-          Codigo de Sincronizacion: 
-          <span className='bg-back-200/75 rounded-lg py-4 px-12'>{user.telegramId}</span>
-       </div> 
+      <>
+        <div className='w-10/12 px-4 pt-12 mx-auto flex justify-evenly items-center text-lg'> 
+            Codigo de Sincronizacion: 
+            <span className='bg-back-200/75 rounded-lg py-4 px-12'>{user.telegramId}</span>
+        </div>
+        <div>
+          Como Sincronizarse?
+          dirigirse a telegram, ingresar al chat de Rec an Dec, ingresar tu mail y tu codigo de sincronizacion y listo
+        </div>
+      </>
        : ""
       }
+      <div>
+        Como Registrar usando telegram?
+        Para generar un registro en telegram se debe segir la siguiente estructura: [monto] -[alias de categoria]
+        Es importante agregar el -(guion medio) antes del alias de la categoria
+        Si lo que desea es registrar un ingreso la estructura seria la misma pero con un +(mas) adelante del monto
+        Para detallar una billetera se debera agregar luego del monto en cualquier parte el alias de la billetera con la siguiente estructura: $[alias de billetera]
+        Para detallar los tags se debera agregar luego del monto en cualquier parte los alias de las etiquetas con la siguiente estructura: ([alias de tag], [alias de tag])
+      </div>
       <ConfigurationTemplate CreateModal={CommandModal}  tableHead={tableHead} tableBody={tableBody} get={getCommand}
         title={"Comandos"} createTitle={"Nuevo Comando"}/>
     </>
