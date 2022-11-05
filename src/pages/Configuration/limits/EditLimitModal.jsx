dbad9439ce32208551d55189b37eb6922cebd558
Ignacio Garcia <ignacioagustin.garcia@uala.com.ar>
2022-11-05 15:50:12 -0300

    fix edit limit

--- src/pages/Configuration/limits/EditLimitModal.jsx
+++ src/pages/Configuration/limits/EditLimitModal.jsx
@@ -2,33 +2,21 @@ import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../../contexts/UserContext'
 import { ConfigContext } from '../../../contexts/ConfigContext'
 import { useLazyFetch } from '../../../hooks/useLazyFetch'
-import { useSimpleSelect } from '../../../hooks/useSimpleSelect'
 import { Modal } from '../../../components/Modal'
 import { updateLimit } from '../../../api/LimitsService'
 import { useForm } from '../../../hooks/useForm'
-import Select from 'react-select'
-import CustomSelect from '../../../components/CustomSelect'
 
 export const EditLimitModal = ({ toggleOpen, limit }) => {
   console.log(limit)
   const { user } = useContext(UserContext)
   const { categories } = useContext(ConfigContext)
-  const { selected, onSelectChange } = useSimpleSelect(limit.category)
   const { formState, onInputChange } = useForm({
     amount: limit.amount
   })
   const [formError, setError] = useState(null)
   const response = useLazyFetch()
 
-  let gettedList = () => categories.data.filter(el => el.isOut).map(el => {
-    return { value: el._id, label: el.label }
-  })
-
   const changeLimit = () => {
-    if(!selected || selected.length < 0) {
-        setError("Debe seleccionar una categoria")
-        return
-    }
     if(!formState.amount){
         setError("El Monto no puede estar vacio")
         return
@@ -36,29 +24,28 @@ export const EditLimitModal = ({ toggleOpen, limit }) => {
         setError("El Monto no puede ser negativo")
         return
     }
-    response.run(updateLimit(user, {_id: limit._id, amount: formState.amount, category: selected.value}))
+    response.run(updateLimit(user, {_id: limit._id, amount: formState.amount}))
   }
 
   useEffect(() => {
     if(!response.loading && (response.error || response.body)){
         if (response.error) {
             alert("Error al Enviar");
-        } else if (response.body.code === 11000) {
-            setError("Limite ya existente para esa categoria")
         } else {
             alert("Limite Actualizado!");
             toggleOpen(false)
         }
     }
-}, [response.loading])
+  }, [response.loading])
 
   return (
     <Modal onPost={changeLimit} toggleOpen={toggleOpen}>
         <h2 className='title mb-8'> Editar Limite </h2>
         
         <div>
-            <div className='mb-3'>
-                <CustomSelect options={gettedList()} value={selected} onChange={onSelectChange} className="w-1/2 m-auto"/>
+            <div className='mb-3 flex items-center justify-center'>
+                <span className='mr-6'>Categoria:</span>
+                <input className='input bg-back-100 border-back-400' value={ limit.category.label } disabled/>
             </div>
             <div className='mb-8'>
                 <label htmlFor="amount" className='mr-6'>Monto:</label>
